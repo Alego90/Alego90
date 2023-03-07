145d34654d85f498146743b43cd06f7efaf50c1d
Alego90 <91158489+Alego90@users.noreply.github.com>
2023-03-07 10:41:35 -0500

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -1,32 +1,34 @@
-function mostrarOpciones() {
-	var seleccion = document.getElementById("solicitud").value;
-	var opciones = document.getElementById("option");
+const seleccion = document.querySelector("#solicitud")
+
+seleccion.addEventListener("click", (e) => {
+	const opciones = document.getElementById("opcion");
 	opciones.innerHTML = "";
-	switch (seleccion) {
-	  case "Infraestructura":
-		opciones.innerHTML = "<opcion>Puntos de Red</opcion><option>Wifi</option><option>Reubicación de Equipos</option>";
-		break;
-	  case "Información":
-		opciones.innerHTML = "<option>Consultas</option><option>Informes</option>";
-		break;
-	  case "D365-CRM":
-		opciones.innerHTML = "<option>Alta de usuario</option><option>Convertir archivos a Excel</option>";
-		break;
-	  case "Equipos":
-		opciones.innerHTML = "<option>Nuevo PC</option><option>Nuevo Celular</option><option>Revisión técnica de PC o Celular</option>";
-		break;
-	  case "Otros":
-		opciones.innerHTML = "";
-		var comentario = document.createElement("input");
-		comentario.type = "text";
-		comentario.id = "comentario";
-		comentario.name = "comentario";
-		comentario.placeholder = "Ingrese su comentario";
-		opciones.appendChild(comentario);
-		break;
-	  default:
-		opciones.innerHTML = "";
-		break;
+	switch (e.target.value) {
+		case "infraestructura":
+			opciones.innerHTML = "<opcion>Puntos de Red</opcion><option>Wifi</option><option>Reubicación de Equipos</option>";
+			break;
+		case "información":
+			opciones.innerHTML = "<option>Consultas</option><option>Informes</option>";
+			break;
+		case "d365-crm":
+			opciones.innerHTML = "<option>Alta de usuario</option><option>Convertir archivos a Excel</option>";
+			break;
+		case "equipos":
+			opciones.innerHTML = "<option>Nuevo PC</option><option>Nuevo Celular</option><option>Revisión técnica de PC o Celular</option>";
+			break;
+		case "otros":
+			opciones.innerHTML = "";
+			var comentario = document.createElement("input");
+			comentario.type = "text";
+			comentario.id = "comentario";
+			comentario.name = "comentario";
+			comentario.placeholder = "Ingrese su comentario";
+			opciones.appendChild(comentario);
+			break;
+		default:
+			opciones.innerHTML = "";
+			break;
 	}
-  }
+})
+
   
